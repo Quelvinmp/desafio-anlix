cdcfa5d46cd1a2b0d23e6dd93df03e85d3053684
Quelvinmp <quelvinmp@gmail.com>
2023-06-15 13:37:35 -0300

    fix: releases consumption of the entire api to localhost

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 
-const routers = require('./routers')
+const routers = require('./routers');
 
 const app = express();
+
+const corsOptions = {
+  origin: 'http://localhost:5173',
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
